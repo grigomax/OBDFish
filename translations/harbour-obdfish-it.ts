b4eb8857d6d022cb84f0e1bf41cb83f7fd1d9704
Massimo <massimo.grigolin@gmail.com>
2017-01-08 10:15:59 +0100

    Italian File

---
+++ translations/harbour-obdfish-it.ts
@@ -0,0 +1,567 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About OBDFish</source>
+        <translation>Info OBDFish</translation>
+    </message>
+    <message>
+        <source>OBD ELM327 car diagnostic reader application for Sailfish OS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>License: GPLv3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Source code:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Date: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Feedback, bugs:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Dyn1Page</name>
+    <message>
+        <source>Dynamic Values 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cycle time: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The more parameters are requested, the higher the cycle time.&lt;br&gt;To get a more responsive cycle time, go to settings and reduce amount of parameters for this page.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Dyn2Page</name>
+    <message>
+        <source>Dynamic Values 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The more parameters are requested, the higher the cycle time.&lt;br&gt;To get a more responsive cycle time, go to settings and reduce amount of parameters for this page.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cycle time: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Dyn3Page</name>
+    <message>
+        <source>Dynamic Values 3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The more parameters are requested, the higher the cycle time.&lt;br&gt;To get a more responsive cycle time, go to settings and reduce amount of parameters for this page.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cycle time: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ErrorPage</name>
+    <message>
+        <source>Error Informations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Your vehicle does not support the reading of errors.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No errors found!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error codes:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Your vehicle has </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> errors!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Trouble codes successfully cleared.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Clear Trouble Codes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Are you sure?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>NOTE: if you proceed, all trouble code information is immediately lost!&lt;br&gt;Your vehicle may run poorly for a short time, while it performs a recalibration.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Reading values...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Do not show this again</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Do not show this warning again.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Could not clear trouble codes.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error ID&apos;s: 
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GeneralInfo</name>
+    <message>
+        <source>General Informations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Vehicle Identification Number: &lt;br&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>OBD adapter: ELM327 </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>OBD standard: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>OBD protocol: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Battery voltage: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Fuel type: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Reading values...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GeneralSettingsPage</name>
+    <message>
+        <source>General settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Write debug file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Communication with vehicle is saved to a file: /home/nemo/Documents/obd_log.txt</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Welcome to OBDFish</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Scan for Bluetooth devices...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Start Scanning...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Disconnect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Checking OBD adapter...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Disconnected from adapter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error while connecting: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error: unknown adapter. This is no ELM327 device!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Switch echo off...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Switch linefeed off...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Switch headers off...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Set protocol...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Supported PID&apos;s 0101-0120...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Supported PID&apos;s 0121-0140...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Supported PID&apos;s 0141-0160...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Supported PID&apos;s 0900-0920...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Successfully connected to car computer!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Connecting:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Connecting to OBD adapter...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Initializing:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>OBD adapters (press to connect):</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Found adapters (press to connect):</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>General Informations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Dynamic Values</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error Informations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No supported PID&apos;s found!&lt;br&gt;- turn on ignition/engine&lt;br&gt;- reconnect to OBD adapter</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OBDDataObject</name>
+    <message>
+        <source>Engine light, error number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Fuel system 1 and 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Engine Load</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Engine Temp</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Intake Air Pressure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Engine RPM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>rpm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Vehicle Speed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Timing Advance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Intake Air Temp</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Air Flow Rate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>grams/sec</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Throttle Position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Fuel Type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Not available</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Gasoline</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Propane</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Electric</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bifuel running Gasoline</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bifuel running Methanol</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bifuel running Ethanol</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bifuel running LPG</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bifuel running CNG</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bifuel running Propane</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bifuel running Electricity</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bifuel running electric and combustion engine</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hybrid gasoline</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hybrid Electric</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hybrid running electric and combustion engine</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hybrid Regenerative</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bifuel running diesel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Undefined</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>OBD-II as defined by the CARB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>OBD as defined by the EPA</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>OBD and OBD-II</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Not OBD compliant</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>EOBD (Europe)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>EOBD and OBD-II</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>EOBD and OBD</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>EOBD, OBD and OBD II</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>JOBD and OBD II</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>JOBD and EOBD</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>JOBD, EOBD, and OBD II</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Battery voltage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Short term fuel trim, Bank 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Long term fuel trim, Bank 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Short term fuel trim, Bank 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Long term fuel trim, Bank 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Fuel pressure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Run time since engine start</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;br&gt;Open loop due to insufficient engine temperature</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;br&gt;Closed loop, using O2 sensor for fuel mix</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;br&gt;Open loop due to engine load OR fuel cut due to deceleration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;br&gt;Open loop due to system failure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;br&gt;Closed loop, using at least one oxygen sensor but there is a fault in the feedback system</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Empty</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Dynamic Parameters Page: </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
